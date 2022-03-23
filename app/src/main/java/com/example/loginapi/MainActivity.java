cc9c438a87d85858f9d2b8e5b41bf4366c4ac2ac
Shweta <shwetashilwant99@gmail.com>
2022-03-22 19:02:08 -0700

    initial commit

---
+++ app/src/main/java/com/example/loginapi/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.loginapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    // button, textview and progressbar.
+    private EditText nameEdt;
+    private Button postDataBtn;
+    private TextView responseTV;
+    private ProgressBar loadingPB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initializing our views
+        nameEdt = findViewById(R.id.idEdtName);
+        postDataBtn = findViewById(R.id.idBtnPost);
+        responseTV = findViewById(R.id.idTVResponse);
+        loadingPB = findViewById(R.id.idLoadingPB);
+
+        // adding on click listener to our button.
+        postDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // validating if the text field is empty or not.
+                if (nameEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter both the values", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // calling a method to post the data and passing our name and job.
+                postDataUsingVolley(nameEdt.getText().toString());
+
+            }
+        });
+    }
+
+    private void postDataUsingVolley(String  name) {
+        // url to post our data
+        String url = "https://202.143.6.44.1831/api/Mob/Login";
+        loadingPB.setVisibility(View.VISIBLE);
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        // on below line we are calling a string
+        // request method to post the data to our API
+        // in this we are calling a post method.
+        StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // inside on response method we are
+                // hiding our progress bar
+                // and setting data to edit text as empty
+                loadingPB.setVisibility(View.GONE);
+                nameEdt.setText("");
+
+                // on below line we are displaying a success toast message.
+                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+                try {
+                    // on below line we are passing our response
+                    // to json object to extract data from it.
+                    JSONObject respObj = new JSONObject(response);
+
+                    // below are the strings which we
+                    // extract from our json object.
+                    String name = respObj.getString("name");
+
+                    // on below line we are setting this string s to our text view.
+                    responseTV.setText("Name : " + name );
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // method to handle errors.
+                Toast.makeText(MainActivity.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // below line we are creating a map for
+                // storing our values in key and value pair.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // on below line we are passing our key
+                // and value pair to our parameters.
+                params.put("name", name);
+
+                // at last we are
+                // returning our params.
+                return params;
+            }
+
+
+           /* @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }*/
+        };
+        // below line is to make
+        // a json object request.
+        queue.add(request);
+    }
+
+}
